d96f97885b04e30e7d6a76a4e53c9047400912ff
ese3633 <ese3633@mail.rit.edu>
2017-04-12 18:26:13 -0400

    added template

---
+++ website/html/template.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Human Resources</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</head>
+<body style="padding-top:60px;">
+    
+<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar">
+    <div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand">Human Resources</a>
+    </div>
+
+    <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li><a>Dashboard</a></li>
+            <li><a>View Employees</a></li>
+            <li><a>blah blah</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a>Logout</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>  
+    
+    <p>BLAH BLAH</p>
+    
+<footer>
+    <p>HumanResources 2017</p>
+</footer>
+</body>
+</html>
