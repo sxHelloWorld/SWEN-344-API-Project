f4fe2a30ed72e1158d4cf010445519f783be00ff
JKotowicz <jtk7412@rit.edu>
2017-04-26 16:08:55 -0400

    added a button for viewing info (not functional)

---
+++ website/html/employeeProfile.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Human Resources</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body style="padding-top:60px;">
+    
+<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar">
+    <div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand">Human Resources</a>
+    </div>
+
+    <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li><a>Dashboard</a></li>
+            <li><a>View Employees</a></li>
+            <li><a>blah blah</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a>Logout</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>  
+
+<div id="buttonLinks">
+    <button id="view-info" class="btn-info">View Profile info</button>
+</div>
+    
+<footer>
+    <p>HumanResources 2017</p>
+</footer>
+</body>
+</html>
