327882c79544abbac35eb72068d42590658b03d9
jeremy <jxv6455@rit.edu>
2017-05-07 15:39:46 -0400

    PostPassword.php now also uses headers to get user being edited

--- website/html/php/postPassword.php
+++ website/html/php/postPassword.php
@@ -1,39 +1,53 @@
 <?php
 
 include 'auth.php';
+include 'request.php';
 
-if($AUTH == 0) {
-    header("Location: index.php");
+if($AUTH < 1) {
+    header("Location: ../index.php");
     die();
 }
 
-include 'request.php';
+$addString = "";
+if(isset($_GET['user'])) {
+    $editUser = $_GET['user'];
+    if($editUser != $user) {
+        $addString = "&user=" . $editUser;
+    }
+} else {
+    $editUser = $user;
+}
+
+if($editUser != $user && $AUTH <= 1) {
+    header("Location: ../index.php");
+    die();
+}
 
 if (!(isset($_POST["newPass"]) && isset($_POST["confirmNewPass"]))) {
-	header("Location: ../editPassword.php?msg=0");
+	header("Location: ../editPassword.php?msg=0" . $addString);
 	die();
 }
 
 if (empty($_POST["newPass"]) || empty($_POST["confirmNewPass"])) {
-	header("Location: ../editPassword.php?msg=1");
+	header("Location: ../editPassword.php?msg=1" . $addString);
 	die();
 }
 
 if ($_POST["newPass"] != $_POST["confirmNewPass"]) {
-	header("Location: ../editPassword.php?msg=2");
+	header("Location: ../editPassword.php?msg=2" . $addString);
     die();
 }
 
 $password = $_POST['newPass'];
 
-$array = array("username" => $user, "password" => $password);
+$array = array("username" => $editUser, "password" => $password);
 $data = request("human_resources", "updatePassword", $array);
 
 if($data == "false") {
-    header("Location: ../editPassword.php?msg=0");
+    header("Location: ../editPassword.php?msg=0" . $addString);
     die();
 } else {
-    header("Location: ../editPassword.php?msg=3");
+    header("Location: ../editPassword.php?msg=3" . $addString);
     die();
 }
 
