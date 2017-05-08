6df1ffbfce2c0c5ec4a0923a7dddaa484328f264
JKotowicz <jtk7412@rit.edu>
2017-05-08 18:32:11 -0400

    removed personal comments

--- website/html/php/glassdoorAPI.php
+++ website/html/php/glassdoorAPI.php
@@ -18,9 +18,4 @@ $url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=147776&t.k=gbqk
 echo "<br>";
 //echo $results;
 
-//find code to convert $results XML string to actual XML object, then can do search there
-//do all printing here, combine the code from 2 pages
-
-//make one PHP page to process user input (select 1 from option below), then another page to print results. you will not need to make a unique request every time, you will get the same XML string every time, so don't worry about that. it's like we're cheating. so just make the output look different 
-
 ?>
